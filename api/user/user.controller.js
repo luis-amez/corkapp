ca1977861e94360fd7f91ec9174de215c1c70829
Luis Amez <luis.amez.vitoria@gmail.com>
2017-08-03 13:28:13 +0200

    Fix user schema

--- api/user/user.controller.js
+++ api/user/user.controller.js
@@ -1,17 +1,1 @@
-const mongoose = require('mongoose');
-const Schema = mongoose.Schema;
 
-const userSchema = new Schema ({
-  username: {
-    type: String,
-    require: true
-  },
-  password: {
-    type: String,
-    require: true
-  },
-  email: {
-    type: String,
-    require: true
-  }
-});
