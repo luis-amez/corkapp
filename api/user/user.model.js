1b8b7e6283f4e9372b16fafc4e064813c725c921
Luis Amez <luis.amez.vitoria@gmail.com>
2017-08-03 13:31:48 +0200

    Fix user schema, again

--- api/user/user.model.js
+++ api/user/user.model.js
@@ -14,4 +14,11 @@ const userSchema = new Schema ({
     type: String,
     require: true
   }
+}, {
+  timestamps: {
+    createdAt: "created_at",
+    updatedAt: "updated_at"
+  }
 });
+
+module.exports = mongoose.model('User', userSchema);  
