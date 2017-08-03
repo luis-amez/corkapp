ab72396969be326ddee5ea2024e3611be3f3da3e
Luis Amez <luis.amez.vitoria@gmail.com>
2017-08-03 13:10:04 +0200

    Comment and clean unnecessary files

--- app.js
+++ app.js
@@ -9,15 +9,15 @@ const mongoose = require('mongoose');
 require('dotenv').config();
 
 
-const index = require('./routes/index');
-const users = require('./routes/users');
+// const index = require('./routes/index');
+// const users = require('./routes/users');
 
 const app = express();
 
 mongoose.connect(process.env.MONGODB_URI);
 
 // view engine setup
-app.set('views', path.join(__dirname, 'views'));
+// app.set('views', path.join(__dirname, 'views'));
 app.set('view engine', 'jade');
 
 // uncomment after placing your favicon in /public
@@ -28,15 +28,16 @@ app.use(bodyParser.urlencoded({ extended: false }));
 app.use(cookieParser());
 app.use(express.static(path.join(__dirname, 'public')));
 
-app.use('/', index);
-app.use('/users', users);
+// app.use('/', index);
+// app.use('/users', users);
+require('./routes')(app);
 
 // catch 404 and forward to error handler
-app.use(function(req, res, next) {
-  let err = new Error('Not Found');
-  err.status = 404;
-  next(err);
-});
+// app.use(function(req, res, next) {
+//   let err = new Error('Not Found');
+//   err.status = 404;
+//   next(err);
+// });
 
 // error handler
 app.use(function(err, req, res, next) {
--- routes/index.js
+++ routes/index.js
@@ -1,9 +1,20 @@
-var express = require('express');
-var router = express.Router();
+// var express = require('express');
+// var router = express.Router();
+//
+// /* GET home page. */
+// router.get('/', function(req, res, next) {
+//   res.render('index', { title: 'Express' });
+// });
+//
+// module.exports = router;
 
-/* GET home page. */
-router.get('/', function(req, res, next) {
-  res.render('index', { title: 'Express' });
-});
+const path = require('path');
 
-module.exports = router;
+module.exports = function(app) {
+  app.use('/api/user', require('../api/user'));
+
+	// catch 404 and forward to Angular
+  app.all('/*', function (req, res) {
+    res.sendfile(__dirname + '/public/index.html');
+  });
+};
